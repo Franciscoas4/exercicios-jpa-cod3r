c626f933c1c4aa92c85a924126405415f122e75f
Franciscoas4 <franciscoas4@gmail.com>
2020-09-15 22:45:48 -0300

    Herança em JPA criando uma única tabela com todos os atributos

--- src/modelo/heranca/Aluno.java
+++ src/modelo/heranca/Aluno.java
@@ -6,7 +6,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Aluno {
 
 	@Id
